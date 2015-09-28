9360a48cf90879972cc6c4d5de5f08bad4874139
Raul <raul.quispe@icloud.com>
2015-09-28 12:57:46 -0500

    Data Task

--- RESTConsume/ViewController.swift
+++ RESTConsume/ViewController.swift
@@ -12,18 +12,36 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //https://api.parse.com/1/classes/Product
         // Do any additional setup after loading the view, typically from a nib.
-        let urlPath: String = "YOUR_URL_HERE"
-        var url: NSURL = NSURL(string: urlPath)!
-        var request1: NSURLRequest = NSURLRequest(URL: url)
-        var response: AutoreleasingUnsafeMutablePointer<NSURLResponse?>=nil
-        var dataVal: NSData =  NSURLConnection.sendSynchronousRequest(request1, returningResponse: response, error:nil)!
-        var err: NSError
-        println(response)
-        var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(dataVal, options: NSJSONReadingOptions.MutableContainers, error: &err) as? NSDictionary
-        println("Synchronous\(jsonResult)")
+        let apiKey = "lgaqGDTH1tzWG8R04pK0VpgJ7wIhZFc4hmsDo6HO"
+        let restKey = "Kw7N8gRoZHGDcVI0zt2T9XFejxNNp5KQEv4wMt3F"
+        let urlAPI = "https://api.parse.com/1/"
+        let urlPath: String = (urlAPI + "classes/"+"Product")
+        let url: NSURL = NSURL(string: urlPath)!
+        let request1: NSMutableURLRequest =  NSMutableURLRequest(URL:url)
+        request1.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request1.addValue(apiKey, forHTTPHeaderField: "X-Parse-Application-Id")
+        request1.addValue(restKey, forHTTPHeaderField: "X-Parse-REST-API-Key")
+        
+        let session = NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithRequest(request1, completionHandler: { (data, response, error) -> Void in
+            if error != nil {
+               // println("error: \(error.localizedDescription): \(error.userInfo)")
+            }
+            else if data != nil {
+                if let str = NSString(data: data!, encoding: NSUTF8StringEncoding) {
+                    print("Received data:\n\(str)")
+                }
+                else {
+                    print("unable to convert data to text")
+                }
+            }
+        })
+        
+        task.resume()
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
