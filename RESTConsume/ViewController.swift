a6743092b0742aa87a903da0fb0c4845c6f6bb2d
Raul <raul.quispe@icloud.com>
2015-09-28 11:15:16 -0500

    START

--- RESTConsume/ViewController.swift
+++ RESTConsume/ViewController.swift
@@ -13,6 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let urlPath: String = "YOUR_URL_HERE"
+        var url: NSURL = NSURL(string: urlPath)!
+        var request1: NSURLRequest = NSURLRequest(URL: url)
+        var response: AutoreleasingUnsafeMutablePointer<NSURLResponse?>=nil
+        var dataVal: NSData =  NSURLConnection.sendSynchronousRequest(request1, returningResponse: response, error:nil)!
+        var err: NSError
+        println(response)
+        var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(dataVal, options: NSJSONReadingOptions.MutableContainers, error: &err) as? NSDictionary
+        println("Synchronous\(jsonResult)")
     }
 
     override func didReceiveMemoryWarning() {
